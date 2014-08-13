4eb6812cb3c960e47e05f57e6debd2040199083f
Chinmaya Kuduvalli <kr.chinmaya@gmail.com>
2014-08-13 01:50:40 -0700

    Assignment7: Adding FacePamphlet Canvas and modified FacePamphlet

--- Assignment7/FacePamphlet.java
+++ Assignment7/FacePamphlet.java
@@ -14,8 +14,8 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class FacePamphlet extends Program 
-					implements FacePamphletConstants {
-	
+implements FacePamphletConstants {
+
 	private JButton addButton;
 	private JButton deleteButton;
 	private JButton lookupButton;
@@ -35,10 +35,23 @@ public class FacePamphlet extends Program
 	 * initialization that needs to be performed.
 	 */
 	public void init() {
-		
+
 		this.database = new FacePamphletDatabase();
 		this.canvas = new FacePamphletCanvas();
-		
+
+		createLayout();
+
+		addActionListeners();
+		statusField.addActionListener(this);
+		pictureField.addActionListener(this);
+		addFriendField.addActionListener(this);
+	}
+
+
+	/**
+	 * Creates the profile page layout for the profile being viewed.
+	 */
+	private void createLayout() {
 		this.addButton = new JButton("Add");
 		this.deleteButton = new JButton("Delete");
 		this.lookupButton = new JButton("Lookup");
@@ -49,7 +62,7 @@ public class FacePamphlet extends Program
 		this.statusField = new JTextField(TEXT_FIELD_SIZE);
 		this.pictureField = new JTextField(TEXT_FIELD_SIZE);
 		this.addFriendField = new JTextField(TEXT_FIELD_SIZE);
-		
+
 		add(new JLabel("Name"), NORTH);
 		add(nameField, NORTH);
 		add(addButton, NORTH);
@@ -63,35 +76,101 @@ public class FacePamphlet extends Program
 		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
 		add(addFriendField, WEST);
 		add(addFriendButton, WEST);
-		
+
 		add(canvas);
-		
-		addActionListeners();
-		statusField.addActionListener(this);
-		pictureField.addActionListener(this);
-		addFriendField.addActionListener(this);
-    }
-    
-  
-    /**
-     * This class is responsible for detecting when the buttons are
-     * clicked or interactors are used, so you will have to add code
-     * to respond to these actions.
-     */
-    public void actionPerformed(ActionEvent e) {
-    	
-    	if(e.getActionCommand().equals("Add")) {
-    		
-    	} else if(e.getActionCommand().equals("Delete")) {
-    		
-    	} else if(e.getActionCommand().equals("Lookup")) {
-    		
-    	} else if(e.getActionCommand().equals("Change Status") || e.getSource() == statusField) {
-    		
-    	} else if(e.getActionCommand().equals("Change Picture") || e.getSource() == pictureField) {
-    		
-    	} else if(e.getActionCommand().equals("Add Friend") || e.getSource() == addFriendField) {
-    		
-    	}
-    }
+	}
+
+
+	/**
+	 * This class is responsible for detecting when the buttons are
+	 * clicked or interactors are used, so you will have to add code
+	 * to respond to these actions.
+	 */
+	public void actionPerformed(ActionEvent e) {
+		String profileName = nameField.getText();
+		String status = statusField.getText();
+		String imageName = pictureField.getText();
+		String friendName = addFriendField.getText();
+
+		if(e.getActionCommand().equals("Add")) {
+			if(!profileName.equals("")) {
+				if(database.containsProfile(profileName)) {
+					canvas.displayProfile(database.getProfile(profileName));
+					canvas.showMessage("Profile of " + profileName + " already exists");
+				} else {
+					database.addProfile(new FacePamphletProfile(profileName));
+					canvas.displayProfile(database.getProfile(profileName));
+					canvas.showMessage("New profile created");
+				}
+			}    		
+		} else if(e.getActionCommand().equals("Delete")) {
+			if(!profileName.equals("")) {
+				if(database.containsProfile(profileName)) {
+					database.deleteProfile(profileName);
+					canvas.displayProfile(null);
+					canvas.showMessage("Profile of " + profileName + " deleted");
+				} else {
+					canvas.displayProfile(null);
+					canvas.showMessage("Profile of " + profileName + " does not exist");
+				}
+			}
+		} else if(e.getActionCommand().equals("Lookup")) {
+			if(!profileName.equals("")) {
+				if(database.containsProfile(profileName)) {
+					canvas.displayProfile(database.getProfile(profileName));
+					canvas.showMessage("Displaying " + profileName);
+				} else {
+					canvas.displayProfile(null);
+					canvas.showMessage("Profile of " + profileName + " does not exist");
+				}
+			}
+		} else if(e.getActionCommand().equals("Change Status") || e.getSource() == statusField) {
+			if(!profileName.equals("")) {
+				if(database.containsProfile(profileName)) {
+					database.getProfile(profileName).setStatus(status);
+					canvas.displayProfile(database.getProfile(profileName));
+					canvas.showMessage("Status updated");
+				} else {
+					canvas.displayProfile(null);
+					canvas.showMessage("Profile of " + profileName + " does not exist");
+				}
+			}
+		} else if(e.getActionCommand().equals("Change Picture") || e.getSource() == pictureField) {
+			if(!profileName.equals("")) {
+				GImage image = null;
+				if(database.containsProfile(profileName)) {
+					try {
+						if(!imageName.equals("")) {
+							image = new GImage(imageName);        					
+						}
+						database.getProfile(profileName).setImage(image);
+						canvas.displayProfile(database.getProfile(profileName));
+						canvas.showMessage("Picture Updated");
+					} catch(ErrorException ex) {
+						JOptionPane.showMessageDialog(canvas, imageName + " image could not be found", "Error", JOptionPane.ERROR_MESSAGE);
+					}
+				} else {
+					canvas.displayProfile(null);
+					canvas.showMessage("Profile of " + profileName + " does not exist");
+				}
+			}
+		} else if(e.getActionCommand().equals("Add Friend") || e.getSource() == addFriendField) {
+			if(!profileName.equals("") && !friendName.equals("")) {
+				if(database.containsProfile(profileName) && database.containsProfile(friendName)) {
+					if(!friendName.equals(profileName)) {
+						database.getProfile(profileName).addFriend(friendName);
+						database.getProfile(friendName).addFriend(profileName);
+						canvas.displayProfile(database.getProfile(profileName));
+						canvas.showMessage(friendName + " added as a friend");
+					} else {
+						canvas.displayProfile(null);
+						canvas.showMessage("Please add a profile other than your own");
+					}
+				} else {
+					canvas.displayProfile(null);
+					canvas.showMessage("Profile of " + profileName + " does not exist");
+				}
+			}
+		}
+	}
 }
--- Assignment7/FacePamphletCanvas.java
+++ Assignment7/FacePamphletCanvas.java
@@ -8,19 +8,28 @@
 
 
 import acm.graphics.*;
+
 import java.awt.*;
 import java.util.*;
+
+import sun.org.mozilla.javascript.tools.shell.Global;
 
 public class FacePamphletCanvas extends GCanvas 
 					implements FacePamphletConstants {
 	
+	private GLabel appMessage;
+	private GLabel nameLabel;
+	private GImage image;
+
+
 	/** 
 	 * Constructor
 	 * This method takes care of any initialization needed for 
 	 * the display
 	 */
 	public FacePamphletCanvas() {
-		// You fill this in
+		this.appMessage = new GLabel("");
+		this.image = null;
 	}
 
 	
@@ -31,7 +40,15 @@ public class FacePamphletCanvas extends GCanvas
 	 * passed in.
 	 */
 	public void showMessage(String msg) {
-		// You fill this in
+		remove(appMessage);
+		appMessage = new GLabel(msg);
+		appMessage.setFont(MESSAGE_FONT);
+		
+		double xLabel = getWidth()/2 - appMessage.getWidth()/2;
+		double yLabel = getHeight() - BOTTOM_MESSAGE_MARGIN - appMessage.getDescent();
+		
+		appMessage.setLocation(xLabel, yLabel);
+		add(appMessage);
 	}
 	
 	
@@ -45,8 +62,115 @@ public class FacePamphletCanvas extends GCanvas
 	 * the user, and a list of the user's friends in the social network.
 	 */
 	public void displayProfile(FacePamphletProfile profile) {
-		// You fill this in
+		removeAll();
+		if(profile != null) {
+			displayName(profile.getName());
+			displayImage(profile.getImage());
+			displayStatus(profile.getName(), profile.getStatus());
+			displayFriends(profile.getFriends());
+		}
 	}
 
+	/**
+	 * Displays the name of the profile currently being viewed.
+	 * 
+	 * @param name The name of the profile being viewed
+	 */
+	private void displayName(String name) {
+		nameLabel = new GLabel(name);
+		nameLabel.setFont(PROFILE_NAME_FONT);
+		
+		double xLabel = LEFT_MARGIN;
+		double yLabel = TOP_MARGIN + nameLabel.getAscent();
+		
+		nameLabel.setLocation(xLabel, yLabel);
+		nameLabel.setColor(Color.BLUE);
+		add(nameLabel);
+	}
+
+	/**
+	 * Displays the profile picture of the profile being viewed
+	 * under the profile name.
+	 * 
+	 * @param image The profile picture of the profile.
+	 */
+	private void displayImage(GImage image) {
+		if(image != null) {
+			this.image = image;
+			
+			double yImage = nameLabel.getY()+nameLabel.getDescent() + IMAGE_MARGIN;
+			double xImage = LEFT_MARGIN;
+			
+			this.image.setBounds(xImage, yImage, IMAGE_WIDTH, IMAGE_HEIGHT);
+			add(this.image);
+		} else {
+			double yImageBox = nameLabel.getY()+nameLabel.getDescent() + IMAGE_MARGIN;
+			double xImageBox = LEFT_MARGIN;
+
+			GRect imageBox = new GRect(xImageBox, yImageBox, IMAGE_WIDTH, IMAGE_HEIGHT);
+			add(imageBox);
+			
+			GLabel imageLabel = new GLabel("No Image");
+			imageLabel.setFont(PROFILE_IMAGE_FONT);
+			
+			double xLabel = LEFT_MARGIN + IMAGE_WIDTH/2 - imageLabel.getWidth()/2;
+			double yLabel = yImageBox + IMAGE_HEIGHT/2;
+			
+			imageLabel.setLocation(xLabel, yLabel);
+			add(imageLabel);
+		}
+	}
+
+	/**
+	 * Displays the status of the profile being viewed under the profile picture.
+	 * 
+	 * @param name The name of the profile being viewed
+	 * @param status The status of the profile being viewed
+	 */
+	private void displayStatus(String name, String status) {
+		GLabel statusLabel = new GLabel("");
+		
+		if(!status.equals("")) {
+			statusLabel = new GLabel(name + " is " + status);
+		} else {
+			statusLabel = new GLabel("No current status");
+		}
+		statusLabel.setFont(PROFILE_STATUS_FONT);			
+		
+		double xLabel = LEFT_MARGIN;
+		double yLabel = nameLabel.getY() + nameLabel.getDescent() + IMAGE_MARGIN 
+				+ IMAGE_HEIGHT + STATUS_MARGIN + statusLabel.getAscent();
 	
+		statusLabel.setLocation(xLabel, yLabel);
+		add(statusLabel);
+	}
+
+	/**
+	 * Displays the list of friends the current profile has, given that
+	 * the profiles of all thos friends exist.
+	 * 
+	 * @param friends The iterator of the friends of the current profile
+	 */
+	private void displayFriends(Iterator<String> friends) {
+		GLabel friendsLabel = new GLabel("Friends:");
+		friendsLabel.setFont(PROFILE_FRIEND_LABEL_FONT);
+		
+		double xLabel = getWidth()/2;
+		double yLabel = nameLabel.getY() + nameLabel.getDescent() + IMAGE_MARGIN;
+		
+		friendsLabel.setLocation(xLabel, yLabel);
+		add(friendsLabel);
+		
+		yLabel += friendsLabel.getHeight();
+		
+		while(friends.hasNext()) {
+			GLabel friendsList = new GLabel(friends.next());
+			friendsList.setFont(PROFILE_FRIEND_FONT);
+			
+			yLabel += friendsList.getHeight();
+			
+			friendsList.setLocation(xLabel, yLabel);
+			add(friendsList);
+		}
+	}
 }
